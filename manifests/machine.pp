d7ab31229d5b383faa7543b0a3a58e8b86b058bb
Steve Hunger <steve.hunger@kinneygroup.com>
2018-04-02 12:19:39 -0400

    final code corrections

--- manifests/machine.pp
+++ manifests/machine.pp
@@ -8,14 +8,15 @@
 # @example
 #   include appdynamics_agent::machine
 class appdynamics_agent::machine (
-  $machine_path           = undef,
-  $machine_agent_file_32  = undef,
-  $machine_agent_file_64  = undef,
-  $controller_host        = undef,
-  $controller_port        = undef,
-  $unique_host_id         = undef,
-  $account_access_key     = undef,
-  $account_name           = undef,
+
+  $machine_agent_file_32  = undef, #filename of the RPM package downloaded
+  $machine_agent_file_64  = undef, #filename of the RPM package downloaded
+  $controller_port        = '8090',  #default port is 8090
+  $controller_host        = undef, #IP address of the controller
+  $account_access_key     = undef, #from the controller
+  $account_name           = undef, #from the controller
+  $unique_host_id         = undef, #optional
+  $machine_path           = undef, #optional
   $sim_enabled            = false,
   $ssl_enabled            = false,
   $enable_orchestration   = false,
@@ -23,14 +24,28 @@ class appdynamics_agent::machine (
   $machine_agent          = '/etc/sysconfig/appdynamics-machine-agent',
   $machine_service_name   = 'appdynamics-machine-agent',
 ) {
+  validate_integer($controller_port)
   validate_bool($sim_enabled)
   validate_bool($ssl_enabled)
   validate_bool($enable_orchestration)
   validate_absolute_path($controller_info)
   validate_absolute_path($machine_agent)
+  if ($machine_agent_file_32 == 'undef')  and ($machine_agent_file_64 == 'undef') {
+    fail('No RPM package provided. Follow the module instructions.')
+  }
+  if $controller_host == 'undef' {
+    fail('The Controller IP adderss was not provided.')
+  }
+  if $account_access_key == 'undef' {
+    fail('Access key is not defined. Get the key from the controller.')
+  }
+  if $account_name == 'undef' {
+    fail('Access account is not defined. Get the account name from the controller.')
+  }
 
   if $::facts['os']['family'] == 'RedHat' {
     if $::facts['os']['architecture'] == 'x86_64' {
+      validate_absolute_path($machine_agent_file_64)
       file { '/tmp/appd_machine_agent.rpm':
         ensure => file,
         source => "puppet:///modules/appdynamics_agent/machine_agent/linux_64_rpm/${machine_agent_file_64}";
@@ -43,6 +58,7 @@ class appdynamics_agent::machine (
         require  => File['/tmp/appd_machine_agent.rpm'],
       }
     } elsif $::facts['os']['architecture'] == i386 {
+      validate_absolute_path($machine_agent_file_32)
       file { '/tmp/machine_agent_rpm':
         ensure => file,
         source => "puppet:///modules/appdynamics_agent/machine_agent/linux_32_rpm/${machine_agent_file_32}";
